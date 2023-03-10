149b6e2a49fdaeef0a5a1eeb5c227ef7888fcb1c
MaryGde <goude.marie@outlook.com>
2023-03-10 16:09:24 +0100

    ajout exercises du jour

---
+++ Demo.py
@@ -0,0 +1,25 @@
+chiffre_1 = 4
+print(chiffre_1 + 5)
+neenoo = "namour"
+print(type(neenoo))
+print(type(chiffre_1))
+
+def addition(a: int, b: int): 
+    return(a + b)
+
+print(addition(4, 5))
+
+def soustraction(a: int, b: int):
+    return(a - b)
+
+print(soustraction(6, 3))
+
+def multiplication(a: int, b: int):
+    return(a * b)
+
+print(multiplication(2, 4))
+
+def division(a: int, b: int):
+    return(a / b)
+
+print(division(4, 2))
